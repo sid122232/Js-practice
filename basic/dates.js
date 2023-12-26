b8b48655d8648eb266e34a3f7c10325e87e8d02f
sid122232 <154446013+sid122232@users.noreply.github.com>
2023-12-26 08:49:58 +0000

    change the container confrigration

---
+++ basic/dates.js
@@ -0,0 +1,12 @@
+let myDate= new Date();
+// console.log(myDate)
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString()); Dates comon uses
+let TimeStamp = Date.now(); // It will get you the date in milliseconds from 1st jan 1970
+console.log(TimeStamp);
+//To covert this in seconds then minutes we use following
