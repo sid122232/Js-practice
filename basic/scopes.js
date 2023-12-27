a0826d5cc7443776830d54a228a323b2fa7dafe8
sid122232 <154446013+sid122232@users.noreply.github.com>
2023-12-27 13:19:23 +0000

    ....

--- basic/scopes.js
+++ basic/scopes.js
@@ -1,1 +1,9 @@
-Two types of scopes are global an
+// Two types of scopes are global and block scope
+// The varibles contained inside the {} are aplicable only to block scope like
+const a = (sid)=>{
+    let b = "hey"
+    console.log(sid, b)
+}
+a()
+console.log(a)
+//In this a is global scope it is applicable for page but b is block scope it is appliable only for block scope
